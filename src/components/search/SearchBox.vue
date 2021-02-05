0e1257f953127727f40eda3f53b999b4e46f3d93
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-02-04 22:42:00 -0300

    fix: add autoprefixer

---
+++ src/components/search/SearchBox.vue
@@ -0,0 +1,16 @@
+SearchBox
+<template>
+  <input
+    type="search"
+    v-model="searchInput"
+    v-on:keyup.enter="$emit('fetch-game')"
+    class="flex-1 text-black px-4 py-2 bg-gray-800 text-gray-300 rounded-sm"
+    placeholder="Enter your favorite game..."
+  />
+  <button
+    @click="$emit('fetch-game')"
+    class="py-2 px-5 uppercase font-bold bg-green-400 rounded-sm ml-2"
+  >
+    Search
+  </button>
+</template>
