52a352a9a28f87f5ce774af647a6ee6964c28515
Albert Mir <albert.mir@volcanicinternet.com>
2021-02-13 12:20:48 +0100

    Prevent eslint duplicated key warning

--- src/components/game/GameCard.vue
+++ src/components/game/GameCard.vue
@@ -50,13 +50,10 @@ export default {
     game: Object,
   },
   setup(props) {
-    const { game } = props
     const el = ref(null)
     const { observe, unobserve, isShown } = useIntersectionObserver()
 
-    const platforms = computed(() => {
-      return game.platforms
-    })
+    const platforms = computed(() => props.game.platforms)
 
     onMounted(() => {
       observe(el.value)
@@ -66,7 +63,7 @@ export default {
       unobserve(el.value)
     })
 
-    return { game, platforms, el, isShown }
+    return { platforms, el, isShown }
   },
 }
 </script>
